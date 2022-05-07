1f0fbeec39b5a3b2a5a06db77405739de57e8dba
andreea-dascalu1 <99488563+andreea-dascalu1@users.noreply.github.com>
2022-05-07 20:27:50 +0300

    Added _external.

---
+++ Submarine/Submarine/Submarine.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h> // necesare pentru citirea shaderStencilTesting-elor
+
+#include <GL/glew.h>
+
+#define GLM_FORCE_CTOR_INIT
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+#pragma comment (lib, "assimp-vc140-mt.lib")
+
+int main() {
+	return 0;
+}
