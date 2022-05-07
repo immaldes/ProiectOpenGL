b56b8b5ba00e3fbe13596995bd4f42643a328c53
Anamariaa14 <curea.delia@yahoo.com>
2022-05-07 21:46:23 +0300

    Created camera class.

--- Submarine/Submarine/Submarine.cpp
+++ Submarine/Submarine/Submarine.cpp
@@ -10,11 +10,72 @@
 
 #include <glfw3.h>
 
+#include "Camera.h"
+
 #pragma comment (lib, "glfw3dll.lib")
 #pragma comment (lib, "glew32.lib")
 #pragma comment (lib, "OpenGL32.lib")
 #pragma comment (lib, "assimp-vc140-mt.lib")
 
-int main() {
-	return 0;
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720; 
+
+Camera* pCamera = new Camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0, 0.0, 3.0));
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+bool InitializeWindow(GLFWwindow*& window) {
+	// glfw: initialize and configure
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// glfw window creation
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Submarine", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glewInit();
+
+	// configure global opengl state
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+} 
+
+int main(int argc, char** argv) {
+	
+	GLFWwindow* window;
+	InitializeWindow(window);
+
+	std::string strFullExeFileName = argv[0];
+	//InitializePaths(strFullExeFileName);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {	// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	pCamera->Reshape(width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	pCamera->MouseControl((float)xpos, (float)ypos);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yOffset) {
+	pCamera->ProcessMouseScroll((float)yOffset);
 }
